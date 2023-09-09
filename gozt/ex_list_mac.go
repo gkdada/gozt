a18115ac29fa09bdfae0e51d17f18cbd83ca3f32
Gopal Sagar <gkdada@unknownac87a31fa4b2.attlocal.net>
2023-09-09 14:01:06 -0700

    adding system exclude list for MacOS

---
+++ gozt/ex_list_mac.go
@@ -0,0 +1,13 @@
+//go:build darwin
+// +build darwin
+
+package main
+
+//serves a list of files to exclude from backup for the specified OS
+// these files will be removed from backup if already present.
+//
+//.~lock files are created by libreoffice when editing. These are machine specific and should not be backed up.
+
+func getOsSpecificExcludes() []string {
+	return []string{".localized", "._DS_Store"}
+}
